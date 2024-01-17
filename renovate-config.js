87230c43ff2239723c49b7218b5b535c86aade5d
maduvef <85684029+maduvef@users.noreply.github.com>
2024-01-17 19:11:42 +0100

    Create renovate-config.js

---
+++ renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branchPrefix: 'test-renovate/',
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  repositories: ['maduvef/snapcast-docker']
+};
