c72a749d8d711ef62d308ef5afeafa283cc547f7
maduvef <85684029+maduvef@users.noreply.github.com>
2024-02-02 17:55:00 +0100

    Add new repo

--- renovate-config.js
+++ renovate-config.js
@@ -4,5 +4,5 @@ module.exports = {
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   onboarding: false,
   platform: 'github',
-  repositories: ['maduvef/snapcast-docker']
+  repositories: ['maduvef/snapcast-docker', 'maduvef/yt-dl-proxy']
 };
